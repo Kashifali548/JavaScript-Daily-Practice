3a5bede841ece8595a6991953f983fcb2fce2139
Kashif Ali <120079760+Kashifali548@users.noreply.github.com>
2026-03-18 12:49:56 +0500

    Add files via upload

---
+++ 03. Car Object Information/script.js
@@ -0,0 +1,10 @@
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    color: "White"
+}
+
+console.log("Car Brand " + car.brand);
+
+console.log("This Car is a " + car.year + " " + car.brand + " " + car.model + "and its color is" + car.color);
