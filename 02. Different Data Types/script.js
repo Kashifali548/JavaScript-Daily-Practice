b1472fb4e8efade78d94966dd082965adf01a37c
Kashif Ali <120079760+Kashifali548@users.noreply.github.com>
2026-03-18 12:41:03 +0500

    Add files via upload

---
+++ 02. Different Data Types/script.js
@@ -0,0 +1,9 @@
+let name = "Kashif Ali";
+let age = 23;
+let isStudent = true;
+let skills = ["HTML, CSS, JavaScript"];
+
+console.log("name", "is", typeof name);
+console.log("Age", "is", typeof age);
+console.log("isStudent", "is", typeof isStudent);
+console.log("Skills", "is", typeof skills);
