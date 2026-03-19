4a414eadc65d6cbebf2400954f84a5c90e30b656
Kashif Ali <120079760+Kashifali548@users.noreply.github.com>
2026-03-19 11:14:37 +0500

    Add files via upload

---
+++ 04. Number Checker/script.js
@@ -0,0 +1,13 @@
+let number = -4;
+
+if ( number > 0){
+    console.log("The number is Positive");
+}
+
+if ( number < 0){
+    console.log("The number is Negative");
+}
+
+else {
+    console.log("The number is Zero");
+}
