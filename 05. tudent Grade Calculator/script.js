4e75dde607f1ccddcdb0245cf9d9a08998fd20d7
Kashif Ali <120079760+Kashifali548@users.noreply.github.com>
2026-03-19 11:23:20 +0500

    Add files via upload

---
+++ 05. tudent Grade Calculator/script.js
@@ -0,0 +1,21 @@
+let number = 45;
+
+if (number >= 90){
+    console.log("Grade: A");
+}
+
+else if (number >= 80){
+    console.log("Grade: B");
+}
+
+else if (number >= 70){
+    console.log("Grade: C");
+}
+
+else if (number >= 60){
+    console.log("Grade: D");
+}
+
+else {
+    console.log("Result: Fail");
+}
